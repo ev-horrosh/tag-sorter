4b8e6c738dc23d109ebb5a26d9fdac24b0c82a9d
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2020-11-01 18:16:25 -0500

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# a simple tag sorter
+
+how to use:
+
+- copy hash tags in the txt 
+- run the code
+- locate your txt
+
+your txt will be saved like 'yourtxt_sorted' at the same location
+
+# what it does
+- sorts alphabetically
+- deletes the duplicates 
+- organizes everything in a column
+
+
---
+++ tag sorter.py
@@ -0,0 +1,43 @@
+import os
+from tkinter import Tk
+from tkinter import filedialog
+
+
+root = Tk().withdraw()
+
+
+def open_tags():
+    filepath = filedialog.askopenfilename(title="Select txt")
+    path = os.path.dirname(filepath)
+    filename = os.path.basename(filepath)
+    name = os.path.splitext(filename)[0]+'_sorted.txt'
+    print(path+name)
+
+    with open(filepath, 'r') as list_of_tags:
+        with open(path + '\\' + name, 'w') as list_of_sorted_tags:
+            tags = list_of_tags.readlines()
+            words_no = []
+
+            for words in tags:
+                for w in words.split(" "):
+                    words_no.append(w)
+
+            words_no = sorted(words_no)
+
+            new_list = []
+
+            for s in words_no:
+                if s not in new_list:
+                    new_list.append(s.strip('\n'))
+
+            new_tags = []
+            for i in new_list:
+                if len(i) >= 3:
+                    new_hashtag = i + '\n'
+                    new_tags.append(new_hashtag)
+
+            for t in new_tags:
+                list_of_sorted_tags.write(t)
+
+
+open_tags()
